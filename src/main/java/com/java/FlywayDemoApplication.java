989311567440e15f153bb99692b5567fc19c6b3a
sarita panda <saritapanda1996@gmail.com>
2020-05-15 19:00:42 +0530

    flyway-demo

---
+++ src/main/java/com/java/FlywayDemoApplication.java
@@ -0,0 +1,21 @@
+package com.java;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.configuration.Configuration;
+import org.flywaydb.core.api.configuration.FluentConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class FlywayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayDemoApplication.class, args);
+	}
+
+
+}
